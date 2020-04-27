956f9f71ddb347e49a2d49458ac7f63ef561b496
Raymond <rh@hoogerdijk.org>
2020-04-27 13:32:40 +0200

    Update get-temperature.py

--- get-temperature.py
+++ get-temperature.py
@@ -6,22 +6,22 @@ import glob
 from time import strftime
 
 #change 28-0517c026dbff to the address of your sensor
-temp_sensor = '/sys/bus/w1/devices/28-0517c026dbff/w1_slave'
+temperature_sensor = '/sys/bus/w1/devices/28-0517c026dbff/w1_slave'
 
 def tempRead():
-        t = open(temp_sensor, 'r')
+        t = open(temperature_sensor, 'r')
         lines = t.readlines()
         t.close()
 
-        temp_output = lines[1].find('t=')
-        if temp_output != -1:
-                temp_string = lines[1].strip()[temp_output+2:]
-                temp_c = float(temp_string)/1000.0
-        return round(temp_c,1)
+        temperature_output = lines[1].find('t=')
+        if temperature_output != -1:
+                temperature_string = lines[1].strip()[temperature_output+2:]
+                temperature_c = float(temperature_string)/1000
+        return round(temperature_c,1)
 
 while True:
-    temp = tempRead()
-    print temp
+    temperature = tempRead()
+    print temperature
     datetimeWrite = (time.strftime("%d-%m-%Y ") + time.strftime("%H:%M:%S"))
     print datetimeWrite
     break
